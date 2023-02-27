43801dd9ee562fc4eba1f875042f5c403446b014
Prince Agrawal <surajpsp@gmail.com>
2023-02-27 19:19:42 +0530

    job apply minor fixes

--- app/src/main/java/com/example/jobportal/ui/bottomsheet/congrats_dialog/CongratsBottomsheet.kt
+++ app/src/main/java/com/example/jobportal/ui/bottomsheet/congrats_dialog/CongratsBottomsheet.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.jobportal.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class CongratsBottomsheet : Fragment() {
+class CongratsBottomsheet : BottomSheetDialogFragment() {
 
 
     override fun onCreateView(
